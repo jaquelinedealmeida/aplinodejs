bad20790c8075093e7d926c0252a3784b3198505
Jaqueline de Almeida <jaqueshanti@gmail.com>
2022-04-11 23:38:09 -0300

    aplicacao em nodejs aula rocketseat

---
+++ BestMe.md
@@ -0,0 +1,20 @@
+# Best me app
+
+Encontre sua melhor versão !
+
+Use o App app ao final de cada dia para refletir sobre sua jornada.
+
+# O que vamos aprender?
+A se comunicar com o processo enquanto ele estiver rodando
+
+[] process.stdout;
+[] process.stdin;
+[] process.on
+
+* Assincronismo
+
+node index inicializar
+
+process.stdout.write("Alguma coisa \n\n\n")
+// stdout controle sobre a string
+não precisa usar o return em funções pq o stout write já retorna
---
+++ index.js
@@ -0,0 +1,48 @@
+const questions = [
+    "O que aprendi hoje?",
+    "O que me aborreceu? E o que poderia fazer para melhorar?",
+    "O que me deixou feliz hoje?",
+    "Quantas pessoas ajudei hoje?"
+]
+const ask = (index = 0) => {
+    process.stdout.write("\n\n" + questions[index] + " > ")
+}
+
+ask()
+
+const answers = [] 
+//chamando a pergunta pelo indice
+process.stdin.on("data", data => {
+    answers.push(data.toString().trim())
+    //trim remove espaços vazios
+    if (answers.length < questions.length) {
+        ask(answers.length)
+    }else {
+        process.exit()
+        // exit para o processo
+    }   
+})
+
+process.on('exit', () => {
+    console.log(`
+
+    Bacana, Jaque: 
+
+    O que aprendi hoje foi:
+    ${answers[0]}
+
+    O que me aborreceu? e você poderia melhorar foi?
+    ${answers[1]}
+
+
+    O que me deixou feliz hoje
+    ${answers[2]}
+
+    Você ajudou ${answers[3]} pessoas hoje!!!
+    
+    Volte amanhã para novas reflexões
+    `)
+})
+
+
+//roda a função toda vez que acessa dados
